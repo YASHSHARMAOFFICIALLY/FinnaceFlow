6b7fb7654e16ee5d83e8e4bbeb884d9f447ae6a4
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-14 13:00:08 +0530

    added

--- lib/auth.ts
+++ lib/auth.ts
@@ -22,26 +22,62 @@
 // });
 
 
-import {betterAuth} from "better-auth"
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { db } from "./db";  // We'll set this up next
+// import {betterAuth} from "better-auth"
+// import { prismaAdapter } from "better-auth/adapters/prisma";
+// import { db } from "./db";  // We'll set this up next
+
+// export const auth = betterAuth({
+//     database: prismaAdapter(db, {
+//         provider: "postgresql",
+//     }),
+//     secret: process.env.BETTER_AUTH_SECRET,
+//     emailAndPassword: {
+//         enabled: true
+//     },
+//     socialProviders: {
+//         google: {
+//             clientId: process.env.GOOGLE_CLIENT_ID || "",
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+//         },
+//         github: {
+//             clientId: process.env.GITHUB_CLIENT_ID || "",
+//             clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+//         },
+//     }
+// })
+
+
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { db } from "./db"
 
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
-        provider: "postgresql",
-    }),
-    secret: process.env.BETTER_AUTH_SECRET,
-    emailAndPassword: {
-        enabled: true
+  baseURL: process.env.BETTER_AUTH_URL,
+
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://finveda-neon.vercel.app"
+  ],
+
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+
+  secret: process.env.BETTER_AUTH_SECRET,
+
+  emailAndPassword: {
+    enabled: true
+  },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     },
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-        },
-        github: {
-            clientId: process.env.GITHUB_CLIENT_ID || "",
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-        },
+
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     }
+  }
 })
