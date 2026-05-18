0a7546d790931767c570b78b83da7e3cdc590400
Pratyush Jha <pratyushorjha@gmail.com>
2026-05-18 17:24:57 +0530

    fix: syntax errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,24 @@
-theme: {
-  extend: {
-    keyframes: {
-      marquee: {
-        "0%";  { transform: "translateX(0)" }
-        "100%"; { transform: "translateX(-50%)" }
-      }
-    }
-    animation: {
-      marquee: "marquee 32s linear infinite"
-    }
-  }
-}
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        marquee: 'marquee 32s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
